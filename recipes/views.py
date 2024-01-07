429c8b7bcfbf5f9579a8e35930ccf9241542b4e9
Ladislau Lopes da Silva Junior <101264915+LadislauLopes@users.noreply.github.com>
2024-01-07 18:55:10 -0500

    movendo as urls para o app

---
+++ recipes/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return HttpResponse('TESTADO')
+def contato(request):
+    return HttpResponse('contato')
+def sobre(request):
+    return HttpResponse('sobre')
