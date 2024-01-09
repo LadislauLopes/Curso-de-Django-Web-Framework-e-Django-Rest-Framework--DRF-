6359a61e81b0fa94dddab906f65f3c3a5b751686
LadislauLopes <ladislau.contato.01@gmail.com>
2024-01-09 16:50:47 -0500

    adicionando html de contato

--- recipes/views.py
+++ recipes/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context={'name': 'Ladislau'},)
 def contato(request):
-    return HttpResponse('contato')
+    return render(request,'recipes/contato.html')
 def sobre(request):
     return HttpResponse('sobre')
